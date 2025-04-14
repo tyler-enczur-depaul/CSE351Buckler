7bca5fa0d52b43ae9bf75efb119f23ef7a0c8b87
umer huzaifa <umerhuzaifa@gmail.com>
2025-04-14 17:41:03 -0500

    new code files for threading and multi-tasking

---
+++ software/apps/threads_intro/periodic_logger.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define LOG_INTERVAL_MS 1000
+#define RUNTIME_SECONDS 100
+#define DELAY_THRESH 5
+
+void sleep_ms(int ms) {
+    struct timespec ts = {ms / 1000, (ms % 1000) * 1000000};
+    nanosleep(&ts, NULL);
+}
+
+void* logger_thread(void* arg) {
+    
+    /* Implement a code that runs every LOG_INTERVAL_MS
+    Print a message if it misses an interval by DELAY_THRESH */
+    return NULL;
+}
+
+int main() {
+
+	/* necessary commands for thread creation */
+    return 0;
+}
